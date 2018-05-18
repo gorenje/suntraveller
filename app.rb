6703bf0af4f0ab4b27522095e2c50ee3c16c62a0
Gerrit Riessen <gerrit.riessen@gmail.com>
2018-05-18 07:10:36 +0200

    more pixels

--- app.rb
+++ app.rb
@@ -25,26 +25,33 @@ class PanoApp < Sinatra::Base
   end
 
   get '/pixelart' do
+    redirect "9pixel"
+  end
+
+  get '/9pixel' do
     src = (0..9).to_a + ('a'..'f').to_a
     @random = [].tap {|t| 9.times {6.times {t << src.sample} ; t << "," }}.join
     @content = {
       "Blackhole" => "000,000,000,000,000,000,000,000,000",
       "Sunset" => "000,000,000,000,000,000,000,fffc0b,000",
-      "Noon on clear day" => "3270ed,3270ed,3270ed,3270ed,f0ff00,3270ed,3270ed,3270ed,3270ed",
-      "Noon on overcast day" => "ccc,ccc,ccc,ccc,f0ff00,ccc,ccc,ccc,ccc",
+      "Sunny" => "3270ed,3270ed,3270ed,3270ed,f0ff00,3270ed,3270ed,3270ed,3270ed",
+      "Overcast" => "ccc,ccc,ccc,ccc,f6f2b2,ccc,ccc,ccc,ccc",
       "Sunrise" => "000,000,000,000,000,000,000,ff6d0b,000",
       "Hill" => "000,000,000,000,108a15,000,108a15,108a15,108a15",
       "Mountain" => "000,000,000,000,e4ffe5,000,108a15,108a15,108a15",
       "Horizon" => "3270ed,3270ed,3270ed,3270ed,3270ed,3270ed,108a15,108a15,108a15",
+      "Rain" => "3270ed,ccc,3270ed,ccc,3270ed,ccc,108a15,108a15,108a15",
       "Desert" => "3270ed,f0ff00,3270ed,3270ed,3270ed,3270ed,ffb04a,ffb04a,ffb04a",
       "Ocean view on a clear day" => "3270ed,3270ed,f0ff00,3270ed,3270ed,3270ed,3270ed,3270ed,3270ed",
       "Ocean view on overcast day" => "f0ff00,ccc,ccc,ccc,ccc,ccc,3270ed,3270ed,3270ed",
-      "Candle" => "000,f9cf6a,000,000,fff,000,000,fff,000",
-      "Matchstick" => "000,000,000,fff,fff,f00,000,000,000",
+      "Candle" => "f4f1f8,f9cf6a,f4f1f8,f4f1f8,fff,f4f1f8,f4f1f8,fff,f4f1f8",
+      "Matchstick" => "f4f1f8,f4f1f8,f4f1f8,fff,fff,f00,f4f1f8,f4f1f8,f4f1f8",
       "Fried Egg" => "fff,fff,fff,fff,f0b700,fff,fff,fff,fff",
       "Snowstorm (by night)" => "fff,000,fff,000,fff,000,fff,000,fff",
       "Snowstorm (by day)" => "fff,fff,fff,fff,fff,fff,fff,fff,fff",
-      "Snowstorm (by night) 2" => "000,fff,000,fff,000,fff,000,fff,000",
+      "Cigarette" => "f4f1f8,ccc,f4f1f8,f4f1f8,f4f1f8,ccc,f9cf6a,fff,f00",
+      "Cigar" => "f4f1f8,ccc,f4f1f8,f4f1f8,f4f1f8,ccc,966e0e,966e0e,f00",
+      "Lighthouse" => "fdf62d,f9cf6a,fdf62d,302996,fff,302996,302996,fff,302996",
     }
     haml :pixelart, :layout => false
   end
